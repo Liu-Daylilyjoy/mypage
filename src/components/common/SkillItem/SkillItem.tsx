6090c74db869f0aef5d21cdd2585c712a588fc0c
liudy <joydaylily@gmail.com>
2025-06-10 22:41:42 +0800

    feat(Home): 添加一些个人信息

--- src/components/common/SkillItem/SkillItem.tsx
+++ src/components/common/SkillItem/SkillItem.tsx
@@ -7,9 +7,9 @@ interface SkillItemProps {
 
 export default function SkillItem({ name, level=0 }: SkillItemProps) {
   return (
-    <div className='flex items-center mb-4 h-10'>
+    <div className='flex w-full max-w-3xl items-center mb-4 h-10'>
       <h3 className='text-primary/80 w-32'>{name}</h3>
-      <div className='-z-1 flex-1 h-full border-border border-1 border-solid rounded-md overflow-hidden mb-3'>
+      <div className='flex-1 h-full border-border border-1 border-solid rounded-md overflow-hidden mb-3'>
         <div
           className='w-full h-full transition-all duration-500 bg-gradient-to-r from-red-500/60 via-yellow-500/60 to-green-500/60'
           style={{
