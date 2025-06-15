8e4d723dd7d6e780d9144280d5b538b6e282d342
liudy <joydaylily@gmail.com>
2025-06-15 20:28:10 +0800

    style(blog): 博客列表基础样式

--- src/components/common/Blog/BlogItem.tsx
+++ src/components/common/Blog/BlogItem.tsx
@@ -8,10 +8,19 @@ export interface BlogItemProps {
 
 const BlogItem: React.FC<BlogItemProps> = (blog) => {
   return (
-    <div className="mb-4 w-full bg-secondary/70">
-      <h3 className="text-primary/80 w-32">{blog.title}</h3>
-      <p className="text-primary/80 w-32">{blog.description}</p>
-      <p className="text-primary/80 w-32">{blog.date}</p>
+    <div className="group relative mb-4 w-full bg-secondary/70 p-4 overflow-hidden">
+      <div className="flex flex-col">
+        <h3 className="text-primary/80 truncate">{blog.title}</h3>
+        <p className="text-primary/80">{blog.date}</p>
+      </div>
+
+      <div className="absolute top-0 right-0 h-full w-full bg-red-400 translate-x-full transition-all duration-400 ease-in-out group-hover:translate-x-0" />
+
+      <div className="absolute top-0 right-0 h-full w-full bg-primary translate-x-full transition-all duration-600 ease-in-out group-hover:translate-x-0">
+        <div className="p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+          <p className="text-background line-clamp-2">{blog.description}</p>
+        </div>
+      </div>
     </div>
   )
 }
