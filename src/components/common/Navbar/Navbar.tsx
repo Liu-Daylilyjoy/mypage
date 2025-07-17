e83b546c398bbb592a0fcb7a969f1852b63b0022
Liudy <il521li@vip.qq.com>
2025-07-17 11:39:33 +0800

    style(Navbar): 样式修改，是的导航栏中文字与背景对比度大，以及修复了hover事件有延迟的问题

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -36,58 +36,52 @@ const Navbar = () => {
   const [isVisible, setIsVisible] = useState(true);
   const [isMouseAtTop, setIsMouseAtTop] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  let containerRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
+  const preventScroll = (e: WheelEvent) => {
+    e.preventDefault();
+  };
 
-      // 如果滚动，且鼠标不在顶部，且菜单未展开，则隐藏导航栏
-      if (scrollPosition > 1 && !isMouseAtTop) {
-        setIsVisible(false);
-      } else {
-        setIsVisible(true);
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+
+    // 如果滚动，且鼠标不在顶部，且菜单未展开，则隐藏导航栏
+    if (scrollPosition > 1 && !isMouseAtTop) {
+      setIsVisible(false);
+    } else {
+      setIsVisible(true);
+    }
+  }
+
+  const handleMouseMove = (e: MouseEvent) => {
+    // 如果鼠标在屏幕顶部 100px 范围内
+    if (e.clientY <= 100) {
+      setIsMouseAtTop(true);
+      setIsVisible(true);
+      containerRef.current?.classList.add('show');
+    } else {
+      setIsMouseAtTop(false);
+      // 如果鼠标离开顶部，且菜单未展开，则隐藏导航栏
+      if (window.scrollY > 1) {
+        setIsVisible(isOpen);
       }
     }
+  };
 
-    const handleMouseMove = (e: MouseEvent) => {
-      // 如果鼠标在屏幕顶部 100px 范围内
-      if (e.clientY <= 100) {
-        setIsMouseAtTop(true);
-        setIsVisible(true);
-      } else {
-        setIsMouseAtTop(false);
-        // 如果鼠标离开顶部，且菜单未展开，则隐藏导航栏
-        if (window.scrollY > 1) {
-          setIsVisible(isOpen);
-        }
-      }
-    };
-
+  useEffect(() => {
+    if (!containerRef.current) return;
+    // 鼠标悬停在navbar上时禁止滚动
+    const container = containerRef.current;
+    container.addEventListener('wheel', preventScroll, { passive: false });
     window.addEventListener('scroll', handleScroll, { passive: true });
     window.addEventListener('mousemove', handleMouseMove, { passive: true });
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
       window.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, [isOpen, isMouseAtTop]);
-
-  let containerRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    if (!containerRef.current) return;
-    // 鼠标悬停在navbar上时禁止滚动
-    const container = containerRef.current;
-
-    const preventScroll = (e: WheelEvent) => {
-      e.preventDefault();
-    };
-
-    container.addEventListener('wheel', preventScroll, { passive: false });
-
-    return () => {
       container.removeEventListener('wheel', preventScroll);
     };
-  }, []);
+  }, [containerRef.current]);
 
   return (
     <div
@@ -103,11 +97,10 @@ const Navbar = () => {
         justify-end
         items-center
         px-8
-        bg-transparent
         transition-all
         duration-300
         ${isVisible ? 'translate-y-0' : '-translate-y-full'}
-        navbar-blur
+        ${isMouseAtTop ? 'bg-background/40 backdrop-blur-[3px] shadow-[0_0px_7px_0_var(--color-shadow)]' : 'bg-transparent'}
         `}>
       <div className="absolute left-4">
         <svg className="signature max-md:hidden" width="90" height="60" viewBox="0 0 334 186" fill="none" xmlns="http://www.w3.org/2000/svg">
